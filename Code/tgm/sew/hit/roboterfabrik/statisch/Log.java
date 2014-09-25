84e1c28308a36a8cbf4749d0127fd627408cdeef
serceg-tgm <serceg@student.tgm.ac.at>
2014-09-25 21:07:34 +0200

    edit the function "add" in class "Log"

--- Code/tgm/sew/hit/roboterfabrik/statisch/Log.java
+++ Code/tgm/sew/hit/roboterfabrik/statisch/Log.java
@@ -5,7 +5,25 @@ public class Log {
 	private static Dateizugriff logFile;
 
 	public static synchronized void add(String text) {
-
+		Option oLogVerzeichnis = obuilder.withLongName("logs").withShortName("o").withRequired(true).withDescription("Verzeichnis des Log-Files")
+				.withArgument(abuilder.withName("verzeichnis").withMinimum(1).withMaximum(1).create()).create();
+		
+		if(cl.hasOption(oLogVerzeichnis)) {
+			String pfad = (String) cl.getValue(oLogVerzeichnis);
+			File temp = new File(pfad);
+			if(!temp.exists())temp.mkdirs();
+			if(temp.exists() && temp.isDirectory()){
+				try{
+					File f=new File(pfad+File.separator+"log.txt");
+					f.createNewFile();
+				}catch(Exception e){
+					e.printStackTrace();
+					throw new IllegalArgumentException("Ungueltiger Pfad fuer den Log-Ordner");
+				}
+				this.logVerzeichnis = pfad;
+			}else
+				throw new IllegalArgumentException("Ungueltiger Pfad fuer den Log-Ordner!");
+		}
 	}
 
 }
