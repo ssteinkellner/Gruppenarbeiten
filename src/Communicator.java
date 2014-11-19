cb6d00a35a5f06d835a068e0c4122e6947730e4f
ssteinkellner <Sebastian Steinkellner>
2014-11-19 09:37:33 +0100

    communicator angepasst

--- src/Communicator.java
+++ src/Communicator.java
@@ -1,23 +1,27 @@
+/**
+ * Eine Klasse, die das letzte glied der Decorator kette darstellt,
+ * um für die 2 Richtungen verschiedene längen zu ermöglichen
+ * @author Steinkellner Sebastian
+ * @version 2014.11.19
+ */
 public class Communicator implements Recieveable, Sendable {
 
-	private Translator translator;
-
-	private BadWordFilter badWordFilter;
-
+	private Sendable translator;
+	private Recieveable badWordFilter;
 
 	/**
 	 * @see Recieveable#recieve()
 	 */
 	public String recieve() {
-		return null;
+		return badWordFilter.recieve();
 	}
 
 
 	/**
 	 * @see Sendable#send(java.lang.String)
 	 */
-	public void send(String test) {
-
+	public void send(String text) {
+		translator.send(text);
 	}
 
 }
