7988f1b7307984774af302bd17f7db1cac28ccb1
ssteinkellner <Sebastian Steinkellner>
2014-11-19 23:00:49 +0100

    gui angepasst

--- src/GUI.java
+++ src/GUI.java
@@ -1,70 +1,75 @@
-import javax.swing.JFrame;
-
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Label;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class GUI extends JFrame {
-public JButton send;
+	private JButton send, connect, options;
+	private JTextField text;
 	
-	public JButton connect;
+	private Label chatverlauf;
+	private JScrollPane chatScrollPane;
 	
-	public JButton options;
+	private JComboBox<String> comboBox;
 	
-	public JTextField text;
+	private JPanel oben, unten;
 	
-	public Label chatverlauf;
-	
-	JFrame f;
-	
-	public JComboBox<String> comboBox;
 	public GUI(){
 
-		this.f = new JFrame("Chat");
-		this.f.setSize(600,400);
-		f.getContentPane().setLayout(null);
-		f.setLocationRelativeTo(null);
-		
-		text = new JTextField();
-		text.setText("Insert your text here");
-		text.setBounds(30, 280, 540, 20);
-		f.getContentPane().add(text);
+		this.setTitle("Chat | SSteinkellner, Akölbl");
+		this.setSize(600,400);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		oben = new JPanel();
+		unten = new JPanel();
 		
 		chatverlauf = new Label();
 		chatverlauf.setText("Chatverlauf");
-		chatverlauf.setBounds(30,10, 540, 250);
-		JScrollPane jScrollPane = new JScrollPane(chatverlauf);
-		jScrollPane.setBounds(30, 50, 222, 100);
-		jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		f.getContentPane().add(chatverlauf);
+		chatverlauf.setMinimumSize(new Dimension(540, 250));
+		chatScrollPane = new JScrollPane(chatverlauf);
+		chatScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
+		text = new JTextField();
+		text.setText("Insert your text here");
 		
 		send = new JButton("Send");
 		send.setBounds(30, 320, 150, 36);
-		f.getContentPane().add(send);
 		
 		options = new JButton("Options");
 		options.setBounds(420, 320, 150, 36);
-		f.getContentPane().add(options);
 		
 		JButton connect = new JButton("Connect");
 		connect.setBounds(230, 320, 150, 36);
-		f.getContentPane().add(connect);
-		
 
+		Container cp = this.getContentPane();
 		
+		cp.setLayout(new BorderLayout());
+		oben.setLayout(new GridLayout(1,2));
+		unten.setLayout(new BorderLayout());
+		
+		cp.add(oben, BorderLayout.NORTH);
+		cp.add(chatScrollPane);
+		cp.add(unten, BorderLayout.SOUTH);
+		
+		oben.add(options);
+		oben.add(connect);
+		
+		unten.add(text);
+		unten.add(send, BorderLayout.EAST);
 
-		
-		
-		this.f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setResizable(false);
-		f.setVisible(true);
+		this.setVisible(true);
 	}
 	
+	public static void main(String[] args){
+		new GUI();
+	}
 }
