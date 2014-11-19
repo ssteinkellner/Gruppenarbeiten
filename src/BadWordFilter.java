b8317e3adca0a2dee00acec7b26235bc638f37fb
Alexander Kölbl <alexander.koelbl@student.tgm.ac.at>
2014-11-19 15:57:47 +0100

    BadWordFilter und Translator Logik erstellt

--- src/BadWordFilter.java
+++ src/BadWordFilter.java
@@ -1,15 +1,41 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class BadWordFilter implements Recievable, Activatable {
 
 	private Recievable recievable;
 	
 	private boolean enabled;
-
+	
+	private ArrayList<String> ersetzen;
+	
+	public BadWordFilter(){
+		ersetzen= new ArrayList<String>();
+		ersetzen.add("ficken");
+		ersetzen.add("Arschloch");
+		ersetzen.add("Faggot");
+	}
 
 	/**
 	 * @see Recievable#recieve()
 	 */
 	public String recieve() {
-		return null;
+		String text = recievable.recieve();
+		if(enabled == true){
+			Iterator<String> i = ersetzen.iterator();
+			String temp,temp2;
+			String sterne;
+			while(i.hasNext()){
+				temp = i.next();
+				sterne = "";
+				for(int j = 0; j < (temp.length()-2); j++){
+					sterne = sterne + "*";
+				}
+				temp2 = temp.charAt(0) + sterne + temp.charAt(temp.length());
+				text = text.replaceAll(temp, temp2);
+			}
+		}
+		return text;
 	}
 
 
--- src/Translator.java
+++ src/Translator.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * 
  * @author Alex
@@ -8,13 +11,28 @@ public class Translator implements Sendable, Activatable {
 	private Sendable sendable;
 	
 	private boolean enabled;
-
+	
+	private ArrayList<String[]> ersetzen;
+	
+	public Translator(){
+		ersetzen= new ArrayList<String[]>();
+		ersetzen.add(new String[]{"lächeln","LOL"});
+		ersetzen.add(new String[]{"laecheln","LOL"});
+	}
 
 	/**
 	 * @see Sendable#send(java.lang.String)
 	 */
-	public void send(String test) {
-
+	public void send(String text) {
+		if(enabled == true){
+			Iterator<String[]> i = ersetzen.iterator();
+			String[] temp;
+			while(i.hasNext()){
+				temp = i.next();
+				text = text.replaceAll(temp[0], temp[1]);
+			}
+		}
+		sendable.send(text);
 	}
 
 
