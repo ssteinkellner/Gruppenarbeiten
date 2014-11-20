6ecf2195f079a5e558e79b87039f952371087ca1
ssteinkellner <Sebastian Steinkellner>
2014-11-20 19:46:18 +0100

    output klasse. einfacher auf log umschreiben

--- src/common/Output.java
+++ src/common/Output.java
@@ -3,10 +3,30 @@ package common;
 public class Output {
 	
 	public static void println(String text){
-		System.out.println(text);
+		System.out.println("[OUTPUT]\t" + text);
 	}
 
 	public static void println(Number number){
-		System.out.println(""+number);
+		println(""+number);
+	}
+	
+	public static void debug(String text){
+		String pos = getPosition();
+		System.out.println("[DEBUG][" + pos + "]\t" + text);
+	}
+
+	public static void debug(Number number){
+		debug(""+number);
+	}
+	
+	public static void error(String text){
+		String pos = getPosition();
+		System.err.println("[ERROR][" + pos + "]\t" + text);
+	}
+	
+	private static String getPosition(){
+		StackTraceElement ste = Thread.currentThread().getStackTrace()[3];
+		String pos = ste.getClassName() + ":" + ste.getLineNumber();
+		return pos;
 	}
 }
