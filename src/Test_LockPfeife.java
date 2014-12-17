03388952930c17c363a33b2f821b5b7f442c6322
ssteinkellner <Sebastian Steinkellner>
2014-12-17 08:46:02 +0100

    aktueller status

---
+++ src/Test_LockPfeife.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Test_LockPfeife {
+
+	public Test_LockPfeife() {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/Test_QuakZaehler.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * die klasse sollte den quackzaehler testen.<br />
+ * da aber im quackzaehler die zeahlvariable static gesetzt ist
+ * und die testklasse zuerst die zweite methode aufruft,
+ * die 2 mal quackt, gibt der erste test dann einen fehler,
+ * da sich der zaehler nicht resetten laesst
+ * @author SSteinkellner
+ * @version 2014.12.17
+ */
+public class Test_QuakZaehler {
+	Quakfaehig e;
+	QuakZaehler z;
+
+	@Before
+	public void setUp() throws Exception {
+		e = new GummiEnte();
+		z = new QuakZaehler(e);
+	}
+	
+	@Test
+	public void TestNoQuaks() {
+		assertEquals(0, QuakZaehler.getQuaks());
+	}
+	
+	@Test
+	public void TestTwoQuaks() {
+		z.quaken();
+		z.quaken();
+		assertEquals(2, QuakZaehler.getQuaks());
+	}
+
+}
