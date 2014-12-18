a0d9d477858b785b8c507e20669c70a979578992
ssteinkellner <Sebastian Steinkellner>
2014-12-18 17:07:25 +0100

    code-anpassungen
    einrueckungen und generics

--- src/Quakologe.java
+++ src/Quakologe.java
@@ -1,11 +1,20 @@
-
+/**
+ * implementierter beobachter
+ * @author TLins
+ */
 public class Quakologe implements Beobachter {
-		
-		public void aktualisieren(QuakBeobachtungsSubjekt ente) {
-			System.out.println("Quakologe: " + ente + " hat gerade gequakt.");
-		}
-		
-		public String toString() {
-			return "Quakologe";
-		}
+
+	/**
+	 * @see Beobachter#aktualisieren(QuakBeobachtungsSubjekt)
+	 */
+	public void aktualisieren(QuakBeobachtungsSubjekt ente) {
+		System.out.println("Quakologe: " + ente + " hat gerade gequakt.");
+	}
+
+	/**
+	 * gibt den typ des beobachters als String zurueck
+	 */
+	public String toString() {
+		return "Quakologe";
+	}
 }
