970249b56f3673966c6e06d2a02cc3ea91b8c622
ssteinkellner <Sebastian Steinkellner>
2015-01-07 21:46:28 +0100

    output von anderem projekt kopiert

--- src/common/Output.java
+++ src/common/Output.java
@@ -1,5 +1,66 @@
 package common;
 
-public class Output {
 
+/**
+ * eine klasse für die verschiedenen ausgaben
+ * erleichtert die einbindung von anderen ausgabemedien (zb. eine loglibrary)
+ * @author Steinkellner Sebastian
+ * @version 2014.11.20
+ */
+public class Output {
+	
+	/**
+	 * gibt einen text aus
+	 * @param text text der ausgegeben werden soll
+	 */
+	public static void println(String text){
+		System.out.println("[OUTPUT]\t" + text);
+	}
+
+	/**
+	 * gibt eine zahl aus
+	 * @param number zahl die ausgegeben werden soll
+	 */
+	public static void println(Number number){
+		println(""+number);
+	}
+	
+	/**
+	 * gibt eine debugnachricht im folgenden format aus
+	 * <br>[DEBUG][KlassenName:ZeilenNummer] Nachricht
+	 * @param text text der ausgegeben werden soll
+	 */
+	public static void debug(String text){
+		String pos = getPosition();
+		System.out.println("[DEBUG][" + pos + "]\t" + text);
+	}
+
+	/**
+	 * gibt eine debugnachricht im folgenden format aus
+	 * <br>[DEBUG][KlassenName:ZeilenNummer] Zahl
+	 * @param number Zahl die ausgegeben werden soll
+	 */
+	public static void debug(Number number){
+		debug(""+number);
+	}
+	
+	/**
+	 * gibt eine fehlernachricht im folgenden format aus
+	 * <br>[ERROR][KlassenName:ZeilenNummer] Nachricht
+	 * @param text fehler der ausgegeben werden soll
+	 */
+	public static void error(String text){
+		String pos = getPosition();
+		System.err.println("[ERROR][" + pos + "]\t" + text);
+	}
+	
+	/**
+	 * gibt die position der aufrufenden methode zurueck
+	 * @return position der aufrufenden methode
+	 */
+	private static String getPosition(){
+		StackTraceElement ste = Thread.currentThread().getStackTrace()[3];
+		String pos = ste.getClassName() + ":" + ste.getLineNumber();
+		return pos;
+	}
 }
