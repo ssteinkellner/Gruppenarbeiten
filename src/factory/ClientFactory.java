dd9f1acbde79e9b5f29f2f6c914f64798e899fd2
ssteinkellner <Sebastian Steinkellner>
2015-01-07 21:31:27 +0100

    factorys implementiert

--- src/factory/ClientFactory.java
+++ src/factory/ClientFactory.java
@@ -1,25 +1,33 @@
 package factory;
 
+import server.Balancer;
+import server.PiServer;
 import exceptions.NotAvailableException;
 import interfaces.Createable;
 import interfaces.Factory;
 
+/**
+ * factory, die einen client erstellt
+ * @author SSteinkellner
+ * @version 2015.01.07
+ */
 public class ClientFactory implements Factory {
-
 
 	/**
 	 * @see interfaces.Factory#create(java.lang.String)
 	 */
 	public Createable create(String name) throws NotAvailableException{
+		switch(name.toLowerCase()){
+		case("simple"): return new PiServer();
+		case("graphic"): return new Balancer();
+	}
 		return null;
 	}
-
 
 	/**
 	 * @see interfaces.Factory#getTypeList()
 	 */
 	public String getTypeList() {
-		return null;
+		return "simple, graphic";
 	}
-
 }
--- src/factory/ServerFactory.java
+++ src/factory/ServerFactory.java
@@ -1,25 +1,33 @@
 package factory;
 
-import exceptions.NotAvailableException;
 import interfaces.Createable;
 import interfaces.Factory;
+import server.Balancer;
+import server.PiServer;
+import exceptions.NotAvailableException;
 
+/**
+ * factory, die einen server erstellt
+ * @author SSteinkellner
+ * @version 2015.01.07
+ */
 public class ServerFactory implements Factory {
-
 
 	/**
 	 * @see interfaces.Factory#create(java.lang.String)
 	 */
 	public Createable create(String name) throws NotAvailableException{
+		switch(name.toLowerCase()){
+			case("piserver"): return new PiServer();
+			case("balancer"): return new Balancer();
+		}
 		return null;
 	}
-
 
 	/**
 	 * @see interfaces.Factory#getTypeList()
 	 */
 	public String getTypeList() {
-		return null;
+		return "PiServer, Balancer";
 	}
-
 }
