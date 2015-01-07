1805d1456c7100ee7f96d2ba9f9a495d58394e97
PoecherReneTGM <rpoecher@student.tgm.ac.at>
2015-01-07 19:53:37 +0100

    add PI Methode

--- src/server/PiServer.java
+++ src/server/PiServer.java
@@ -13,8 +13,32 @@ public class PiServer implements Calculator, Createable {
 	 */
 	public BigDecimal pi(int anzahl_nachkommastellen) {
 
+		BigDecimal sum = new BigDecimal (0);
+		for(int i=0; i<anzahl_nachkommastellen; i++){
+			BigDecimal bd = new BigDecimal (i);
+			BigDecimal two = new BigDecimal (2);
+			BigDecimal one = new BigDecimal (1);
+			BigDecimal temp = new BigDecimal (0);
+			BigDecimal minone = new BigDecimal (-1);
 
-		return null;
+			if(i%2 == 0) { // if the remainder of `i/2` is 0
+
+				temp = bd.multiply(two);
+				temp = temp.subtract(one);
+				temp = minone.divide(temp);
+				sum = sum.add(temp);
+
+			}
+			else {
+
+				temp = bd.multiply(two);
+				temp = temp.subtract(one);
+				temp = one.divide(temp);
+				sum = sum.add(temp);
+
+			}
+		}
+	return sum;
 	}
 
 }
