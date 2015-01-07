aba47b6679951ffde9c0c06a1403513d1dda53f5
PoecherReneTGM <rpoecher@student.tgm.ac.at>
2015-01-07 20:19:50 +0100

    add pi test case

--- src/test/PiServerTest.java
+++ src/test/PiServerTest.java
@@ -1,6 +1,10 @@
 package test;
 
+import com.sun.corba.se.spi.activation.Server;
 import org.junit.Test;
+import server.PiServer;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +12,9 @@ public class PiServerTest {
 
     @Test
     public void testPi() throws Exception {
-
+        PiServer test = new PiServer();
+        BigDecimal ret = test.pi(2);
+        BigDecimal testc = new BigDecimal(3.14);
+        assertEquals(testc,ret);
     }
 }
