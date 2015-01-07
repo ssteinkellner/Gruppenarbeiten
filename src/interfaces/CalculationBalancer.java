03af8a50909fb156025ccc1cf3c21e69627575b9
ssteinkellner <Sebastian Steinkellner>
2015-01-07 21:36:48 +0100

    javadoc

--- src/interfaces/CalculationBalancer.java
+++ src/interfaces/CalculationBalancer.java
@@ -1,9 +1,23 @@
 package interfaces;
 
+/**
+ * interface fuer einen balancer, mit register und remove methode
+ * (annaeherung ans Observer Pattern)
+ * @author SSteinkellner
+ * @version 2015.01.07
+ */
 public interface CalculationBalancer {
 
+	/**
+	 * registrierung eines servers, der seinen dienst bereitstellt 
+	 * @param calculator
+	 */
 	public abstract void registerCalculator(Calculator calculator);
 
+	/**
+	 * entfernen eines servers, der seinen dienst nicht laenger bereitstellt
+	 * @param calculator
+	 */
 	public abstract void removeCalculator(Calculator calculator);
 
 }
