220263d49d09b1bfcc30b49a3d60c416b650ed88
PoecherReneTGM <rpoecher@student.tgm.ac.at>
2015-01-08 18:14:38 +0100

    fix kommastellen
    add protokoll + uml

--- src/server/PiServer.java
+++ src/server/PiServer.java
@@ -13,15 +13,14 @@ public class PiServer implements Calculator, Server {
 	 */
 	public BigDecimal pi(int anzahl_nachkommastellen) {
 
-
 		BigDecimal sum2=new BigDecimal(0);
 		int komma= 0;
-		for (int i = 0; komma < anzahl_nachkommastellen+2; i++) {
+		for (int i = 0; komma < anzahl_nachkommastellen; i++) {
 			komma=sum2.toString().length();
 			BigDecimal j = new BigDecimal(i);
 			BigDecimal x = new BigDecimal(0);
 			x=(new BigDecimal(1).divide(new BigDecimal(16).pow(i),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN)).multiply(new BigDecimal(4).divide(new BigDecimal(8).multiply(j).add(new BigDecimal(1)),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN).subtract(new BigDecimal(2).divide(new BigDecimal(8).multiply(j).add(new BigDecimal(4)),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN)).subtract(new BigDecimal(1).divide(new BigDecimal(8).multiply(j).add(new BigDecimal(5)),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN)).subtract(new BigDecimal(1).divide(new BigDecimal(8).multiply(j).add(new BigDecimal(6)),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN)));
-			sum2=x.add(sum2);
+			sum2=x.add(sum2).setScale(anzahl_nachkommastellen,BigDecimal.ROUND_DOWN);
 		}
 
 		return sum2;
