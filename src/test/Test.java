c095663fba4415b74ffa8646fb2a33112ae73a06
PoecherReneTGM <rpoecher@student.tgm.ac.at>
2015-01-08 16:25:44 +0100

    edit  pi alg

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,6 @@
 package test;
 
-import java.lang.Math;
+import java.math.BigDecimal;
 
 /**
  * Created by rene on 07.01.15.
@@ -20,6 +20,59 @@ public class Test {
         }
 
 
-        System.out.print(sum);
+        System.out.println(sum);
+
+
+
+
+
+
+        BigDecimal eins = new BigDecimal (1);
+        BigDecimal zwei = new BigDecimal (2);
+        BigDecimal vier = new BigDecimal (4);
+        BigDecimal fünf = new BigDecimal (5);
+        BigDecimal sechs = new BigDecimal (6);
+        BigDecimal acht = new BigDecimal (8);
+        BigDecimal zehnsechs = new BigDecimal (16);
+        BigDecimal sum1 = new BigDecimal (0);
+        BigDecimal tempx = new BigDecimal (0);
+        BigDecimal temp1 = new BigDecimal (0);
+        BigDecimal temp2 = new BigDecimal (0);
+        BigDecimal temp3 = new BigDecimal (0);
+        BigDecimal temp4 = new BigDecimal (0);
+
+
+
+        for (int i = 0; i < 10 ; i++) {
+
+
+            BigDecimal j = new BigDecimal(i+1);
+            tempx=eins.divide(zehnsechs.pow(i + 1));
+
+            temp1=acht.multiply(j);
+            temp1=temp1.add(eins);
+            temp1=vier.divide(temp1,20,BigDecimal.ROUND_DOWN);
+
+            temp2=acht.multiply(j);
+            temp2=temp2.add(vier);
+            temp2=zwei.divide(temp2,20,BigDecimal.ROUND_DOWN);
+
+            temp3=acht.multiply(j);
+            temp3=temp3.add(fünf);
+            temp3=eins.divide(temp3,20,BigDecimal.ROUND_DOWN);
+
+            temp4=acht.multiply(j);
+            temp4=temp4.add(sechs);
+            temp4=eins.divide(temp4,20,BigDecimal.ROUND_DOWN);
+
+            sum1 = temp1.subtract(temp2).subtract(temp3).subtract(temp4);
+            sum1 = temp1.multiply(sum1);
+        }
+        System.out.println(sum1);
+
+
+
+
+
     }
 }
