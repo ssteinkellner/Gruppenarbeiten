d5fde2654918d9b740b88d64e0f54d0c7dc9ae44
PoecherReneTGM <rpoecher@student.tgm.ac.at>
2015-01-08 17:24:29 +0100

    wefefwe

--- src/server/PiServer.java
+++ src/server/PiServer.java
@@ -15,12 +15,16 @@ public class PiServer implements Calculator, Createable {
 
 
 		BigDecimal sum2=new BigDecimal(0);
-		for (int i = 0; i < 100; i++) {
+		int komma= 0;
+		for (int i = 0; komma < anzahl_nachkommastellen+2; i++) {
+			komma=sum2.toString().length();
 			BigDecimal j = new BigDecimal(i);
 			BigDecimal x = new BigDecimal(0);
 			x=(new BigDecimal(1).divide(new BigDecimal(16).pow(i),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN)).multiply(new BigDecimal(4).divide(new BigDecimal(8).multiply(j).add(new BigDecimal(1)),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN).subtract(new BigDecimal(2).divide(new BigDecimal(8).multiply(j).add(new BigDecimal(4)),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN)).subtract(new BigDecimal(1).divide(new BigDecimal(8).multiply(j).add(new BigDecimal(5)),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN)).subtract(new BigDecimal(1).divide(new BigDecimal(8).multiply(j).add(new BigDecimal(6)),anzahl_nachkommastellen,BigDecimal.ROUND_DOWN)));
 			sum2=x.add(sum2);
 		}
+
+		return sum2;
 	}
 
 }
