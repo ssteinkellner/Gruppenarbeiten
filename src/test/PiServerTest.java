a884ed8d9f3639149a72486c01e0ee5f4d047547
PoecherReneTGM <rpoecher@student.tgm.ac.at>
2015-01-08 16:26:45 +0100

    a

--- src/test/PiServerTest.java
+++ src/test/PiServerTest.java
@@ -1,16 +1,15 @@
 package test;
 
-import com.sun.corba.se.spi.activation.Server;
-import org.junit.Test;
 import server.PiServer;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PiServerTest {
 
-    @Test
+
+
     public void testPi() throws Exception {
         PiServer test = new PiServer();
         BigDecimal ret = test.pi(5);
